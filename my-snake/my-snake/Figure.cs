1572c34b484e0420a0ab7a8784e08bc6ba3675ea
rustamizh <sobakachalova@gmail.com>
2016-02-10 11:16:21 +0400

    создан базовый класс Фигуры для классов горизонтальные и вертикальные линии

---
+++ my-snake/my-snake/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_snake
+{
+    class Figure
+    {
+       protected List<Point> pList;
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
