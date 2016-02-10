661242d2c04f0c9f91a1f491d73bb74359b3f84b
rustamizh <sobakachalova@gmail.com>
2016-02-10 11:04:18 +0400

    добавлены строки по взаимодействию с классами горизонтальных и вертикальных линий

--- my-snake/my-snake/Program.cs
+++ my-snake/my-snake/Program.cs
@@ -16,30 +16,11 @@ namespace my_snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            VerticalLine vLine = new VerticalLine(6, 9, 2, '+');
+            vLine.Drow();
 
             Console.ReadLine();
         }     
