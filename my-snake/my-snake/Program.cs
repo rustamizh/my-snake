aaa1864fd35333b2dd35e4cdb1f0e3f45bfbbca1
rustamizh <sobakachalova@gmail.com>
2016-02-10 12:20:00 +0400

    добавлена отрисовка рабочек и змейки

--- my-snake/my-snake/Program.cs
+++ my-snake/my-snake/Program.cs
@@ -10,18 +10,23 @@ namespace my_snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
 
-            VerticalLine vLine = new VerticalLine(6, 9, 2, '+');
-            vLine.Drow();
 
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
             Console.ReadLine();
         }     
     }
